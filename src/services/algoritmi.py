3a02086d0fd0510bf1521f1a695465944d122afd
J-Uhero <juho.herranen@helsinki.fi>
2022-03-25 20:48:37 +0200

    A* improved and works fine

--- src/services/algoritmi.py
+++ src/services/algoritmi.py
@@ -1,5 +1,4 @@
 from heapq import heappop, heappush
-from queue import Empty
 import math
 
 m1 = [
@@ -9,28 +8,51 @@ m1 = [
     [0, 0, 0, 1, 0]
     ]
 
+m2 = [
+    [0, 1, 0, 0, 0],
+    [0, 0, 1, 0, 0],
+    [0, 1, 1, 0, 0],
+    [0, 0, 0, 1, 0]
+    ]
+
+koord_lisaykset_1 = [(0,-1), (0,1), (-1,0), (1,0)]
+koord_lisaykset_sqrt2 = [(-1,-1), (-1,1), (1,-1), (1,1)]
+
 def reitti(edeltajat, solmu):
+    """haetaan algoritmin reitti edeltajat-hajautustaulusta"""
     r = [solmu]
     while solmu in edeltajat.keys():
         solmu = edeltajat[solmu]
-        r = solmu + r
+        r = [solmu] + r
     return r
 
+def luo_reittimatriisi(m, reitti):
+    uusi_m = list(m)
+    for k in reitti:
+        uusi_m[k[1]][k[0]] = 2
+    return uusi_m
+
+def tulosta_matriisi(m):
+    for i in range(len(m)):
+        print(m[i])
+
 def h_arvot(mista, minne):
-    #h-arvo lasketaan suorana etäisyytenä päätepisteeseen pythagoran lauseella
+    """h-arvo lasketaan suorana etäisyytenä päätepisteeseen pythagoran lauseella
+    """
     return math.sqrt((mista[0]-minne[0])**2 + (mista[1]-minne[1])**2)
 
-def tarkista_sivukoordinaatit(solmu, m):
+def tarkista_koordinaatit(solmu, m, koord_lisaykset):
     p = []
-    for k in [(0,-1), (0,1), (-1,0), (1,0)]:
+    for k in koord_lisaykset:
         uusi = (k[0]+solmu[0], k[1]+solmu[1])
-        if uusi[0] >= 0 and uusi[1] >= 0:
-            if m[solmu[1],solmu[0]] == 0:
+        if (len(m[0]) > uusi[0] >= 0) and (len(m) > uusi[1] >= 0):
+            if m[uusi[1]][uusi[0]] == 0:
                 p.append(uusi)
     return p
 
 def a_tahti(mista, minne, h=h_arvot, m=m1):
-    """Tämä on ensimmäinen versioni a*-algoritmista, joka vielä vaiheessa (key error)
+    """A*-algoritmi. Näyttäisi toimivan nyt oikein.
+    Vaatinee vielä optimointia ja toisteisen koodin siismistä
     """
     keko = []
     heappush(keko, (0, mista)) #(f-arvo, (x,y))
@@ -41,32 +63,33 @@ def a_tahti(mista, minne, h=h_arvot, m=m1):
     f[mista] = h(mista, minne)
     sqrt2 = math.sqrt(2)
 
-    while keko is not Empty:
+    while len(keko) > 0:
         nyk = heappop(keko)
+        print(nyk)
         if nyk[1] == minne:
             return reitti(edeltajat, minne)
-        for k in [(0,-1), (0,1), (-1,0), (1,0)]:
-            print("matriisi arvo",m[k[1]+nyk[1][1]][k[0]+nyk[1][0]], k[1]+nyk[1][1], k[0]+nyk[1][0])  
-            if m[k[1]+nyk[1][1]][k[0]+nyk[1][0]] == 0:
-                seur_k = (k[1]+nyk[1][1],k[0]+nyk[1][0])
-                mahd_g = g[nyk[1]] + 1
-                if mahd_g < g[seur_k]:
-                    g[seur_k] = mahd_g
-                    f_arvo = mahd_g + h(seur_k, minne)
-                    f[seur_k] = f_arvo
-                    edeltajat[seur_k] = nyk[1]
-                    if (f_arvo, seur_k) not in keko:
-                        heappush(keko, (f_arvo, seur_k))
 
-        for k in [(-1,-1), (-1,1), (1,-1), (1,1)]:
-            if m[k[1]+nyk[1][1]][k[0]+nyk[1][0]] == 0:
-                seur_k = (k[1]+nyk[1][1],k[0]+nyk[1][0])
-                mahd_g = g[nyk[1]] + sqrt2
-                if mahd_g < g[seur_k]:
-                    g[seur_k] = mahd_g
-                    f_arvo = mahd_g + h(seur_k, minne)
-                    f[seur_k] = f_arvo
-                    edeltajat[seur_k] = nyk[1]
-                    if (f_arvo, seur_k) not in keko:
-                        heappush(keko, (f_arvo, seur_k))
+        for seur_k in tarkista_koordinaatit(nyk[1], m, koord_lisaykset_1):
+            mahd_g = g[nyk[1]] + 1
+            if seur_k not in g.keys():
+                g[seur_k] = math.inf
+            if mahd_g < g[seur_k]:
+                g[seur_k] = mahd_g
+                f_arvo = mahd_g + h(seur_k, minne)
+                f[seur_k] = f_arvo
+                edeltajat[seur_k] = nyk[1]
+                if (f_arvo, seur_k) not in keko:
+                    heappush(keko, (f_arvo, seur_k))
+
+        for seur_k in tarkista_koordinaatit(nyk[1], m, koord_lisaykset_sqrt2):
+            mahd_g = g[nyk[1]] + sqrt2
+            if seur_k not in g.keys():
+                g[seur_k] = math.inf
+            if mahd_g < g[seur_k]:
+                g[seur_k] = mahd_g
+                f_arvo = mahd_g + h(seur_k, minne)
+                f[seur_k] = f_arvo
+                edeltajat[seur_k] = nyk[1]
+                if (f_arvo, seur_k) not in keko:
+                    heappush(keko, (f_arvo, seur_k))
     return None
