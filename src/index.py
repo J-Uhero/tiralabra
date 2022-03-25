92bb9b7df43a55f75c40e121507e20fb586a0a47
J-Uhero <juho.herranen@helsinki.fi>
2022-03-25 12:07:08 +0200

    basic repository file structure base created

---
+++ src/index.py
@@ -0,0 +1,8 @@
+from ui.ui import defaul_ui
+
+def main():
+    ui = defaul_ui
+    ui.start()
+
+if __name__ == "__main__":
+    main()
---
+++ src/ui/ui.py
@@ -0,0 +1,8 @@
+class Ui:
+    def __init__(self):
+        pass
+
+    def start():
+        pass
+
+default_ui = Ui()
