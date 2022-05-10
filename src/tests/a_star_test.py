d00804d9a36a6a1cfc36c74be52ad0e333dff5df
J-Uhero <juho.herranen@helsinki.fi>
2022-05-10 15:54:05 +0300

    repared a_star_test.py

--- src/tests/a_star_test.py
+++ src/tests/a_star_test.py
@@ -18,23 +18,13 @@ class TestAlgoritmi(unittest.TestCase):
                           [0,0,0,0,0,0]]
 
     def test_a_tahti_paasee_paamaaraan_eika_palauta_nonea(self):
-        palautus = self.a_star.a_star((5,0), (0,5), m=self.matriisi2)
+        palautus = self.a_star.aloita((5,0), (0,5), m=self.matriisi2)
         self.assertIsNotNone(palautus)
 
     def test_a_tahti_palauttaa_oikeat_koordinaatit(self):
-        koordinaatit = self.a_star.a_star((0,0), (4,4), m=self.matriisi1)[0]
+        koordinaatit = self.a_star.aloita((0,0), (4,4), m=self.matriisi1)[0]
         self.assertEqual([(0,0), (1,1), (1,2), (2,3), (3,3), (4,4)], koordinaatit)
     
     def test_a_tahti_palauttaa_none_kun_reittia_ei_loydy(self):
-        palautus = self.a_star.a_star((4,0), (0,4), m=self.matriisi1)
+        palautus = self.a_star.aloita((4,0), (0,4), m=self.matriisi1)
         self.assertEqual(None, palautus)
-
-    """
-    Tässä kokeilin erilaisten matriisien toimivuutta ja pääsenkö A*-algoritmin haaroihin
-
-    def test_hakea_reitti_joka_kay_umpikujassa(self):
-        palautus = a_tahti((0,27),(2,27), m=self.matriisi3)
-        print(palautus)
-        palautus2 = a_tahti((5,0), (0,5), m=self.matriisi2)
-        #self.assertEqual(None, palautus)
-    """
